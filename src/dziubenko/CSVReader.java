e23f584a4ce43b770b1049f60717048d46bd5b90
smthelusive <smthelusive@gmail.com>
2017-07-15 03:20:25 +0200

    hang fix

--- src/dziubenko/CSVReader.java
+++ src/dziubenko/CSVReader.java
@@ -14,9 +14,9 @@ import java.util.stream.Stream;
  * Created by Nataliia Dziubenko on 13.07.2017
  */
 public class CSVReader {
-    private static boolean done;
+    private static boolean done = false;
     private final static int NUM = 10000;
-    final static BlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
+    volatile static BlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
 
     public static void main(String[] args) {
         CSVReader reader = new CSVReader();
@@ -42,21 +42,23 @@ public class CSVReader {
         }
         long before = System.currentTimeMillis();
         ExecutorService executor = Executors.newFixedThreadPool(NUM);
-        for (int i = 0; i < NUM; i++) {
-            executor.submit(new Task());
-        }
+        int counter = 0;
         Path file = Paths.get(path);
         try {
             Stream<String> lines = Files.lines(file, StandardCharsets.UTF_8);
             for(String line : (Iterable<String>)lines :: iterator) {
                 queue.put(line);
+                if (counter < NUM) {//create once and not more than needed
+                    executor.submit(new Task());
+                    counter++;
+                }
             }
             done = true;
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         } finally {
-            executor.shutdown();
             try {
+                executor.shutdown();
                 executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
                 System.out.println(System.currentTimeMillis() - before + " ms");
             } catch (InterruptedException e) {
